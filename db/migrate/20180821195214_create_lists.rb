b8d0f53f08fa0df136f41079f45b0ad95e43a937
Emily Welsch <emwelsch@gmail.com>
2018-08-21 20:25:43 +0000

    Create migration folder and files

---
+++ db/migrate/20180821195214_create_lists.rb
@@ -0,0 +1,9 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.text :name
+      t.integer :user_id
+      t.timestamps null: false
+    end
+  end
+end
