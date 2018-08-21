efd239b360ac702b09bf0d952ab9941ac3ab7e51
Emily Welsch <emwelsch@gmail.com>
2018-08-21 20:19:56 +0000

    create application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "victor's_secret"
+    set :session_secret, "psst_its_a_secret"
   end
 
   get "/" do
