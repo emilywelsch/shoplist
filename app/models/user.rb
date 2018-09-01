0716289f00e322af00b8cc3814190c83fdf03544
emilywelsch <emwelsch@gmail.com>
2018-08-31 21:55:29 -0400

    Update item links

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   has_many :items
   has_secure_password
-  validates_presence_of :email, :username
+  validates_presence_of :email
+  validates_uniqueness_of :username
 
   def slug
     username.downcase.gsub(" ","-")
